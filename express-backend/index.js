ec5873d4e9a1e6154a8c442736aeed9fed557c9c
Jacob Nyman <jacobnyman@Jacobs-MacBook-Pro.local>
2020-07-14 16:52:58 +0200

    change uri

--- express-backend/index.js
+++ express-backend/index.js
@@ -16,12 +16,15 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Hello World"));
-
+console.log(
+  path.resolve(__dirname, "..", "practice-app", "build", "index.html")
+);
 mongoose
   .connect(
-    process.env.NODE_ENV === "production"
-      ? dbconfig.mongoURIProd
-      : dbconfig.mongoURI,
+    // process.env.NODE_ENV === "production"
+    //   ? dbconfig.mongoURIProd
+    //   : dbconfig.mongoURI,
+    dbconfig.mongoURIProd,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
